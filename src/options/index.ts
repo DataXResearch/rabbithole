4408baed4887e289c00126f5c68db08e31277ac0
Govind Mohan <gov@supercluster.dev>
2023-09-01 00:06:39 -0400

    refactor: remove template code

--- src/options/index.ts
+++ src/options/index.ts
@@ -1,12 +1,9 @@
 import Options from "src/lib/Options.svelte";
-import { storage } from "src/storage";
 
 const target = document.getElementById("app");
 
 function render() {
-  storage.get().then(({ count }) => {
-    new Options({ target, props: { count } });
-  });
+  new Options({ target });
 }
 
 document.addEventListener("DOMContentLoaded", render);
