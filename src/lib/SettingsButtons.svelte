050d7f57f5ce6a53b552c3ab526d5095a3a618de
Govind Mohan <gov@supercluster.dev>
2023-09-01 23:34:37 -0400

    feat: allow settings change from new tab

---
+++ src/lib/SettingsButtons.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { Button, Group, Tooltip } from '@svelteuidev/core';
+  import Options from "./Options.svelte";
+  import { MessageRequest } from "../utils.ts"
+
+  export let settings: Settings = {
+    show: false,
+    alignment: "right"
+  };
+  let isHovering = false;
+
+  function changeAlignment(event) {
+    if (settings.alignment === "left") {
+      settings.alignment = "right";
+    } else {
+      settings.alignment = "left";
+    }
+    chrome.runtime.sendMessage({
+      type: MessageRequest.UPDATE_SETTINGS,
+      settings,
+    });
+  }
+
+  async function hideOverlay(event) {
+    if (settings.show === true) {
+      settings.show = false;
+    } else {
+      settings.show = true;
+    }
+    chrome.runtime.sendMessage({
+      type: MessageRequest.UPDATE_SETTINGS,
+      settings,
+    });
+  }
+
+  onMount(async () => {
+    settings = await chrome.runtime.sendMessage({type: MessageRequest.GET_SETTINGS});
+  });
+</script>
+
+<div class="buttons">
+  <Group position="center" spacing="md">
+    <Button on:click={changeAlignment} id="move" variant='light' color='blue'>
+      {#if settings.alignment === "right"}
+        Move button left
+      {:else}
+        Move button right
+      {/if}
+    </Button>
+    <Button
+      on:click={hideOverlay}
+      on:mouseenter={()=>{isHovering=true}}
+      on:mouseleave={()=>{isHovering=false}}
+      id="move"
+      variant='light'
+      color='blue'
+      >
+      {#if settings.show}
+        Hide
+      {:else}
+        Show
+      {/if}
+    </Button>
+  </Group>
+</div>
+
+<style>
+  .buttons {
+    margin-top: 10px;
+  }
+</style>
--- src/newtab/index.ts
+++ src/newtab/index.ts
@@ -1,8 +1,10 @@
 import Timeline from "src/lib/Timeline.svelte";
+import SettingsButtons from "src/lib/SettingsButtons.svelte"
 
 const target = document.getElementById("app");
 
 function render() {
+  new SettingsButtons({ target });
   new Timeline({ target });
 }
 
