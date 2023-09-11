0e03d5d9da531098959c4596313ad9bb71783e5f
Govind Mohan <gov@supercluster.dev>
2023-09-11 18:25:50 -0400

    feat: add selector for switching active rabbithole, input/button for new rabbithole creation

--- src/lib/Timeline.svelte
+++ src/lib/Timeline.svelte
@@ -1,22 +1,80 @@
 <script>
   import { onMount } from "svelte";
-  import { Badge, Button, Card, Group, Image, Text } from '@svelteuidev/core';
+  import { Badge, Button, Card, Group, Image, Text, NativeSelect, TextInput } from '@svelteuidev/core';
   import { MessageRequest } from "../utils"
   import TimelineCard from "src/lib/TimelineCard.svelte";
 
+  // FIXME: why aren't types working here?
+
   let websites = [];
+  let projects = [];
   let loading = true;
+  let value = ""; // TODO: see if this can be changed to an accurate name
+  let activeProject = {};
 
   onMount(async () => {
     websites = await chrome.runtime.sendMessage({type: MessageRequest.GET_ALL_ITEMS});
+    // TODO: load active project and set title as default value on the dropdown
+    activeProject = await chrome.runtime.sendMessage({ type: MessageRequest.GET_ACTIVE_PROJECT })
+    refreshProjects();
+    refreshWebsites();
     loading = false;
   });
+
+  async function refreshWebsites() {
+    websites = await chrome.runtime.sendMessage({
+      type: MessageRequest.GET_PROJECT_SAVED_WEBSITES,
+      projectId: activeProject.id,
+    });
+  }
+
+  async function refreshProjects() {
+    projects = await chrome.runtime.sendMessage({type: MessageRequest.GET_ALL_PROJECTS});
+  }
+
+  async function handleProjectChange(event) {
+    await chrome.runtime.sendMessage({
+      type: MessageRequest.CHANGE_ACTIVE_PROJECT,
+      projectId: event.target.value,
+    });
+    activeProject = await chrome.runtime.sendMessage({
+      type: MessageRequest.GET_PROJECT,
+      projectId: event.target.value,
+    })
+    refreshWebsites();
+  }
+
+  async function createNewProject() {
+    await chrome.runtime.sendMessage({
+      type: MessageRequest.CREATE_NEW_PROJECT,
+      newProjectName: value,
+    });
+    refreshProjects();
+  }
 </script>
 
 <div class="timeline">
   <img src="../assets/icons/logo.png">
   <div>
-    </div>
+    <NativeSelect data={projects.map(p => { return { label: p.name, value: p.id }; })}
+                  label="Active Project"
+                  on:change={handleProjectChange}
+                  />
+      <div>
+        <TextInput
+          placeholder="My new rabbithole"
+          label="New Project"
+          bind:value
+          />
+        <Button
+          on:click={createNewProject}
+          variant='light'
+          color='blue'
+          >
+          Create
+        </Button>
+      </div>
+  </div>
   <div class="feed">
   {#if !loading}
     {#each websites as site}
