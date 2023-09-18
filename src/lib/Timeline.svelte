2df53ec44fdb8d3c161c379d4e06cef869566082
Govind Mohan <gov@supercluster.dev>
2023-09-18 18:34:38 -0400

    fix: dedup website list from frontend for now

--- src/lib/Timeline.svelte
+++ src/lib/Timeline.svelte
@@ -22,10 +22,18 @@
   });
 
   async function refreshWebsites() {
-    websites = await chrome.runtime.sendMessage({
+    // FIXME: when rabbithole is installed, the first time a session is saved
+    // the website list is duplicated, so dedup here for now
+    const possiblyDuplicatedWebsites = await chrome.runtime.sendMessage({
       type: MessageRequest.GET_PROJECT_SAVED_WEBSITES,
       projectId: activeProject.id,
     });
+    websites = possiblyDuplicatedWebsites.filter((value, index, self) =>
+      index === self.findIndex((t) => (
+        t.url === value.url
+      ))
+    )
+    console.log(websites)
   }
 
   async function refreshProjects() {
