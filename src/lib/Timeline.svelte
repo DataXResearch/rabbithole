3a8e7e60fdfd66bd8c013b683a7c7e4524b494b6
Govind Mohan <gov@supercluster.dev>
2023-10-25 17:52:51 -0400

    feat: dispatch events to parent element (Rabbithole)

--- src/lib/Timeline.svelte
+++ src/lib/Timeline.svelte
@@ -1,23 +1,17 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount, createEventDispatcher } from "svelte";
   import { Badge, Button, Card, Group, Image, Text, TextInput } from '@svelteuidev/core';
   import { MessageRequest } from "../utils"
   import TimelineCard from "src/lib/TimelineCard.svelte";
   import ProjectSelector from "src/lib/ProjectSelector.svelte"
 
   // FIXME: why aren't types working here?
+  const dispatch = createEventDispatcher();
 
-  let websites = [];
+  export let activeProject = {};
+  export let websites = [];
+
   let projects = [];
-  let loading = true;
-  let newRabbitholeName = "";
-  let activeProject = {};
-
-  onMount(async () => {
-    websites = await chrome.runtime.sendMessage({type: MessageRequest.GET_ALL_ITEMS});
-    refresh();
-    loading = false;
-  });
 
   function makeActiveProjectFirst() {
     for (let i = 0; i < projects.length; i++) {
@@ -28,7 +22,7 @@
     projects.unshift(activeProject);
   }
 
-  async function refresh() {
+  async function updateItems() {
     activeProject = await chrome.runtime.sendMessage({ type: MessageRequest.GET_ACTIVE_PROJECT })
     // FIXME: when rabbithole is installed, the first time a session is saved
     // the website list is duplicated, so dedup here for now
@@ -44,64 +38,20 @@
   }
 
   async function handleProjectChange(event) {
-    await chrome.runtime.sendMessage({
-      type: MessageRequest.CHANGE_ACTIVE_PROJECT,
-      projectId: event.target.value,
+    dispatch('projectChange', {
+      newProjectId: event.target.value,
     });
-    activeProject = await chrome.runtime.sendMessage({
-      type: MessageRequest.GET_PROJECT,
-      projectId: event.target.value,
-    });
-    refresh();
-  }
-
-  async function createNewProject() {
-    if (newRabbitholeName === "") {
-      // TODO: error modal
-      return;
-    }
-    await chrome.runtime.sendMessage({
-      type: MessageRequest.CREATE_NEW_PROJECT,
-      newProjectName: newRabbitholeName,
-    });
-    refresh();
-  }
-
-  async function saveAllTabsToActiveProject() {
-    const tabs = await chrome.runtime.sendMessage({
-      type: MessageRequest.SAVE_WINDOW_TO_ACTIVE_PROJECT,
-    });
-
-    refresh();
-  }
-
-  async function saveAllTabs() {
-    if (newRabbitholeName === "") {
-      // TODO: error modal
-      return;
-    }
-    await chrome.runtime.sendMessage({
-      type: MessageRequest.SAVE_WINDOW_TO_NEW_PROJECT,
-      newProjectName: newRabbitholeName,
-    });
-
-    refresh();
   }
 </script>
 
 <div class="timeline">
-  <img src="../assets/icons/logo.png">
   <div>
     <ProjectSelector projects={projects} handleProjectChange={handleProjectChange} />
   </div>
   <div class="feed">
-    {#if !loading}
-      {#each websites as site}
-        <TimelineCard website={site} />
-      {/each}
-    {:else}
-      <p> Loading... </p>
-    {/if}
+    {#each websites as site}
+      <TimelineCard website={site} />
+    {/each}
   </div>
 </div>
 
