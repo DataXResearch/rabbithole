04bf95da6bed6e07b2484f9b91bc11a1be064b40
Sal <msalanvarov@gmail.com>
2023-11-10 20:18:51 -0500

    :heavy_plus_sign: tailwind and svelte-navigator deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { crx } from "@crxjs/vite-plugin";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { resolve } from "path";
+import path, { resolve } from "path";
 import { defineConfig } from "vite";
 import manifest from "./manifest.json";
 
@@ -8,10 +8,19 @@ const srcDir = resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    commonjsOptions: {
+      include: ["tailwind.config.js", "node_modules/**"],
+    },
+  },
+  optimizeDeps: {
+    include: ["tailwind-config", "svelte-navigator"],
+  },
   plugins: [svelte(), crx({ manifest })],
   resolve: {
     alias: {
       src: srcDir,
+      "tailwind-config": path.resolve(__dirname, "./tailwind.config.js"),
     },
   },
 });
