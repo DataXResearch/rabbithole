3444b2b0b34c715c13574db82715754a11c97c06
gov <info@govindmohan.com>
2025-11-20 16:34:01 -0500

    feat: popup

---
+++ src/popup/Popup.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Overlay from "src/lib/Overlay.svelte";
+</script>
+
+<div class="popup-wrapper">
+  <Overlay isPopup={true} />
+</div>
+
+<style>
+  .popup-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
