2fe1b8f519a38c71c8cab130f15bc307498a9e0c
gov <info@govindmohan.com>
2026-01-28 14:49:12 -0500

    fix: add cur date to exported rabbithole file

--- src/lib/Rabbithole.svelte
+++ src/lib/Rabbithole.svelte
@@ -202,8 +202,14 @@
     });
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const filename = `rabbithole-${day}-${month}-${year}.json`;
+
     link.href = url;
-    link.setAttribute("download", "rabbithole.json");
+    link.setAttribute("download", filename);
     link.click();
   }
 
