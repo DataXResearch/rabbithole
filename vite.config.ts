fc63dc913b2e065484d3ecea817cd9ab4c25e86a
gov <info@govindmohan.com>
2026-02-12 19:36:51 -0500

    chore: polyfill crypto in browser

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { crx } from "@crxjs/vite-plugin";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import manifest from "./manifest.json";
 
 const srcDir = resolve(__dirname, "src");
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), crx({ manifest })],
+  plugins: [svelte(), crx({ manifest }), nodePolyfills()],
   resolve: {
     alias: {
       src: srcDir,
